2440d26cff4f18d6ebb883158ae99cfb2acec9c7
Gauthier-Clerc François <francois.gauthier-clerc@insa-rennes.fr>
2019-03-09 15:42:15 -0700

    update with Stochastic version and clamp method for LinearBin

--- binary_connect.py
+++ binary_connect.py
@@ -1,45 +1,60 @@
 import torch
 
-class BinaryConnect(torch.autograd.Function):
-    """
-    We can implement our own custom autograd Functions by subclassing
-    torch.autograd.Function and implementing the forward and backward passes
-    which operate on Tensors.
-    """
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
+
+
+class BinaryConnectDeterministic(torch.autograd.Function):
 
     @staticmethod
     def forward(ctx, input):
-        """
-        In the forward pass we receive a Tensor containing the input and return
-        a Tensor containing the output. ctx is a context object that can be used
-        to stash information for backward computation. You can cache arbitrary
-        objects for use in the backward pass using the ctx.save_for_backward method.
-        """
         ctx.save_for_backward(input)
-
         return torch.sign(input)
 
     @staticmethod
     def backward(ctx, grad_output):
-        """
-        In the backward pass we receive a Tensor containing the gradient of the loss
-        with respect to the output, and we need to compute the gradient of the loss
-        with respect to the input.
-        """
         input, = ctx.saved_tensors
         grad_input = grad_output.clone()
-        grad_input[torch.abs(input) > 1.00001] = 0
-        
+        grad_input[torch.abs(input) > 1.001] = 0
+
         return grad_input
 
 
-class LinearBin(torch.nn.Linear):
-    def __init__(self, *args, **kwargs):
-        super(LinearBin, self).__init__(*args, **kwargs)
+class BinaryConnectStochastic(torch.autograd.Function):
 
-    def init_weight(self):
-        torch.nn.init.uniform_(self.weight.data, -1, 1)
+    @staticmethod
+    def forward(ctx, input):
+        ctx.save_for_backward(input)
+        z = torch.rand_like(input, requires_grad=False)
+        p = ((torch.clamp(input, -1, 1) + 1) / 2).pow(2)
+
+        return -1.0 + 2.0 * (z<p).type(torch.FloatTensor).to(device)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input, = ctx.saved_tensors
+        grad_input = grad_output.clone()
+        grad_input[torch.abs(input) > 1.001] = 0
+        return grad_input
+
+
+
+
+
+class LinearBin(torch.nn.Linear):
+    def __init__(self, *args, stochastic=False, **kwargs):
+        super(LinearBin, self).__init__(*args, **kwargs)
+        self.stochastic = stochastic
+
+    def reset_parameters(self):
+        torch.nn.init.uniform_(self.weight, -1, 1)
+        if not self.bias is None:
+            torch.nn.init.uniform_(self.bias, -1 , 1)
+
+    def clamp(self):
+        self.weight.data.clamp_(-1, 1)
+        if not self.bias is None:
+            self.bias.data.clamp_(-1, 1)
 
     def forward(self, input):
-        return torch.nn.functional.linear(input, BinaryConnect.apply(self.weight), BinaryConnect.apply(self.bias))
+        return torch.nn.functional.linear(input, BinaryConnectStochastic.apply(self.weight), None if self.bias is None else BinaryConnectStochastic.apply(self.bias))
 
