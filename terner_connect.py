1984e9af469b62da989234dff1a081e50fb96bfa
Gauthier-Clerc François <francois.gauthier-clerc@insa-rennes.fr>
2019-03-09 15:42:53 -0700

    Add terner torch OP

---
+++ terner_connect.py
@@ -0,0 +1,36 @@
+import torch
+
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
+
+
+class TernerConnectDeterministic(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input):
+        ctx.save_for_backward(input)
+        sign = torch.sign(input)
+        return (sign + torch.sign(input -0.5*sign ))/2
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input, = ctx.saved_tensors
+        grad_input = grad_output.clone()
+        grad_input[torch.abs(input) > 1.001] = 0
+        return grad_input
+
+
+class TernerConnectStochastic(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input):
+        ctx.save_for_backward(input)
+        sign = torch.sign(input)
+        z = torch.rand_like(input, requires_grad=False)
+        return sign - (z>torch.abs(input)).type(torch.FloatTensor).to(device) 
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        input, = ctx.saved_tensors
+        grad_input = grad_output.clone()
+        grad_input[torch.abs(input) > 1.001] = 0
+        return grad_input
+
