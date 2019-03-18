9611afcccd833d6e9c488c058a089bb0690e9d5d
Gauthier-Clerc François <francois.gauthier-clerc@insa-rennes.fr>
2019-03-18 17:21:28 -0600

    remove old source code.

--- layers/binary_layers.py
+++ layers/binary_layers.py
@@ -7,27 +7,6 @@ from math import sqrt
 
 from functions import binary_connect
 
-"""
-class LinearBin(torch.nn.Linear):
-    def __init__(self, *args, stochastic=False, **kwargs):
-        super(LinearBin, self).__init__(*args, **kwargs)
-        self.stochastic = stochastic
-
-    def reset_parameters(self):
-        torch.nn.init.uniform_(self.weight, -1, 1)
-        if not self.bias is None:
-            torch.nn.init.uniform_(self.bias, -1 , 1)
-
-    def clamp(self):
-        self.weight.data.clamp_(-1, 1)
-        if not self.bias is None:
-            self.bias.data.clamp_(-1, 1)
-
-    def forward(self, input):
-        return torch.nn.functional.linear(input, binary_connect.BinaryConnectDeterministic.apply(self.weight),self.bias)
-
-"""
-
 class LinearBin(torch.nn.Module):
     def __init__(self, in_features, out_features, bias=True):
         super(LinearBin, self).__init__()
@@ -75,53 +54,3 @@ class BinarizeConv2d(torch.nn.Conv2d):
 
 
 
-
-class Conv2dBin(torch.nn.Module):
-
-    __constants__ = ['stride', 'padding', 'dilation', 'groups', 'bias']
-
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1,
-                 padding=0, dilation=1, groups=1, bias=True):
-        super(Conv2dBin, self).__init__()
-
-        if in_channels % groups != 0:
-            raise ValueError('in_channels must be divisible by groups')
-        if out_channels % groups != 0:
-            raise ValueError('out_channels must be divisible by groups')
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.kernel_size = kernel_size
-        self.stride = stride
-        self.padding = padding
-        self.dilation = dilation
-        self.transposed = False
-        self.output_padding = _pair(0)
-        self.groups = groups
-
-        self.weight = Parameter(torch.Tensor(
-            out_channels, in_channels // groups, *_pair(kernel_size)))
-
-        if bias:
-            self.bias = Parameter(torch.Tensor(out_channels))
-        else:
-            self.register_parameter('bias', None)
-        
-        self.reset_parameters()
-
-
-    def clamp(self):
-        self.weight.data.clamp_(-1, 1)
-        if not self.bias is None:
-            self.bias.data.clamp_(-1, 1)
-
-
-    def reset_parameters(self):
-        torch.nn.init.uniform_(self.weight, -1, 1)
-        if not self.bias is None:
-            torch.nn.init.uniform_(self.bias, -1 , 1)
-
-
-    @weak_script_method
-    def forward(self, input):
-        return F.conv2d(input, binary_connect.BinaryConnectDeterministic.apply(self.weight), self.bias, self.stride,
-                        self.padding, self.dilation, self.groups)
