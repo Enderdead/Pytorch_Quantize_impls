15dafe29f882b3b1ad41c4b43b99d71a2c87de5b
Gauthier-Clerc François <francois@gauthier-clerc.fr>
2019-04-01 17:27:04 -0600

    Add a scipy sampling version.

--- utils/parameters.py
+++ utils/parameters.py
@@ -1,4 +1,6 @@
 import random
+from scipy.stats import uniform
+from math import log10
 
 class Parameter:
     def __init__(self, name):
@@ -10,18 +12,24 @@ class Parameter:
 class UniformFloat(Parameter):
     def __init__(self, name, min, max):
         Parameter.__init__(self, name)
+        self.generator = uniform(min, max)
         self.params = [min, max]
 
     def apply(self, trial=None):
+        if trial is None:
+            return self.generator.rvs(1)[0]
         return trial.suggest_uniform(self.name, *self.params)
 
 
 class UniformInt(Parameter):
     def __init__(self, name, min, max):
         Parameter.__init__(self, name)
+        self.generator = uniform(min, max)
         self.params = [min, max]
 
     def apply(self, trial=None):
+        if trial is None:
+            return int(self.generator.rvs(1)[0])
         return trial.suggest_int(self.name, *self.params)
 
 
@@ -29,8 +37,11 @@ class UniformLog(Parameter):
     def __init__(self, name, min, max):
         Parameter.__init__(self, name)
         self.params = [min, max]
+        self.generator = uniform(log10(min), log10(max))
 
     def apply(self, trial=None):
+        if trial is None:
+            return 10**(self.generator.rvs(1)[0])
         return trial.suggest_loguniform(self.name, *self.params)
 
 class DiscretParameter(Parameter):
@@ -39,4 +50,6 @@ class DiscretParameter(Parameter):
         self.values = values
 
     def apply(self, trial=None):
-        return trial.suggest_discrete_uniform(self.name, *self.values)
+        if trial is None:
+            return random.choice(self.values)
+        return trial.suggest_categorical(self.name, self.values)
