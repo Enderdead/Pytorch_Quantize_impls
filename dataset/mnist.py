7d0af16ba9643682dbba015d9acd259b96c36599
Gauthier-Clerc François <francois@gauthier-clerc.fr>
2019-04-08 18:52:58 -0600

    increase data worker (on MNIST)

--- dataset/mnist.py
+++ dataset/mnist.py
@@ -1,7 +1,7 @@
 import torch
 import torch.utils.data
 import torchvision
-def get_mnist(batch_size=200, valid_ratio=0.2, directory="/tmp/MNIST", transform_both=None, transform_train=None, transform_valid=None):
+def get_mnist(batch_size=200, valid_ratio=0.2, directory="/tmp/MNIST", transform_both=None, transform_train=None, transform_valid=None, num_workers=2):
     if transform_train is None and transform_both is None:
         if transform_both is None:
             transform_train = torchvision.transforms.Compose([
@@ -26,8 +26,8 @@ def get_mnist(batch_size=200, valid_ratio=0.2, directory="/tmp/MNIST", transform
     main_dataset = torchvision.datasets.MNIST(directory, train=True, download=True, transform=transform_train)
     train_dataset, valid_dataset = torch.utils.data.random_split(main_dataset,[int((1-valid_ratio)*main_dataset.data.size()[0]),int(valid_ratio*main_dataset.data.size()[0])] )
     
-    train_set = torch.utils.data.DataLoader(train_dataset,batch_size=batch_size, shuffle=True)    
-    valid_set = torch.utils.data.DataLoader(valid_dataset,batch_size=batch_size, shuffle=True)    
+    train_set = torch.utils.data.DataLoader(train_dataset,batch_size=batch_size, shuffle=True, num_workers=num_workers)    
+    valid_set = torch.utils.data.DataLoader(valid_dataset,batch_size=batch_size, shuffle=True,num_workers=num_workers)    
 
     test_set = torch.utils.data.DataLoader(
         torchvision.datasets.MNIST(directory, train=False, download=True, transform=transform_valid),
